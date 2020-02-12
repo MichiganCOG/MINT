e930ff976ed48eaa5e4d67a58f00bd8a8beddab1
Madan Ravi Ganesh <madantrg@umich.edu>
2020-02-12 08:03:54 -0500

    Trying to obtain input values to modify layer 1

--- mlp/utils/pruning_utils.py
+++ mlp/utils/pruning_utils.py
@@ -40,23 +40,40 @@ def activations(data_loader, model, device, item_key):
     parents_op  = None
     labels_op   = None
 
-    handles     = []
 
-    get_all_layers(model, handles, item_key)
+    if not(item_key == 'input'):
+        handles     = []
+
+        get_all_layers(model, handles, item_key)
 
     print('Collecting Activations for Layer %s'%(item_key))
 
     with torch.no_grad():
         for step, data in enumerate(data_loader):
             x_input, y_label = data
+
+
             model(x_input.to(device))
 
             if temp_op is None:
-                temp_op        = visualisation[list(visualisation.keys())[0]].cpu().numpy()
+                if not(item_key == 'input'):
+                    temp_op  = visualisation[list(visualisation.keys())[0]].cpu().numpy()
+
+                else:
+                    temp_op  = x_input.cpu().numpy()
+
+                # END IF
+
                 temp_labels_op = y_label.numpy()
 
             else:
-                temp_op        = np.vstack((visualisation[list(visualisation.keys())[0]].cpu().numpy(), temp_op))
+                if not(item_key == 'input'):
+                    temp_op        = np.vstack((visualisation[list(visualisation.keys())[0]].cpu().numpy(), temp_op))
+            
+                else:
+                    temp_op        = np.vstack((x_input.cpu().numpy(), temp_op))
+
+                # END IF
                 temp_labels_op = np.hstack((y_label.numpy(), temp_labels_op))
 
             # END IF 
@@ -95,13 +112,16 @@ def activations(data_loader, model, device, item_key):
         temp_labels_op = None
 
     # Remove all hook handles
-    for handle in handles:
-        handle.remove()    
+    if not(item_key == 'input'):
+        for handle in handles:
+            handle.remove()    
     
-    del visualisation[list(visualisation.keys())[0]]
+        del visualisation[list(visualisation.keys())[0]]
 
     if len(parents_op.shape) > 2:
         parents_op  = np.mean(parents_op, axis=(2,3))
+
+    import pdb; pdb.set_trace()
 
     return parents_op, labels_op
 
